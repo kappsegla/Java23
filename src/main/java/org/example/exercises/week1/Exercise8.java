547ec8b253e0ae536cc5e6ed5eab07baf9ec4237
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-01 14:01:23 +0200

    Add Exercise8

--- src/main/java/org/example/exercises/week1/Exercise8.java
+++ src/main/java/org/example/exercises/week1/Exercise8.java
@@ -1,7 +1,29 @@
 package org.example.exercises.week1;
 
+import java.util.*;
+
 public class Exercise8 {
     public static void main(String[] args) {
-        
+        Integer[] array = {1, 2, 1, 3, 3, 4};
+        int count = 0;
+        for (int i = 0; i < array.length - 2; i += 2) {
+            if (array[i] == array[i + 2] || array[i] == array[i + 3]
+                || array[i + 1] == array[i + 2] || array[i + 1] == array[i + 3]) {
+                count++;
+            }
+
+//            Set<Integer> set = new HashSet<>(List.of(Arrays.copyOfRange(array, i, i + 4)));
+//            if (set.size() != 4)
+//                count++;
+        }
+
+        if (count == array.length / 2 - 1) {
+            System.out.println("Everywhere");
+        } else if (count > 0) {
+            System.out.println("Somewhere");
+        } else
+            System.out.println("Nowhere");
+
+
     }
 }
