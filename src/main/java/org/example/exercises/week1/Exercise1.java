08e1ecc298dab15285acc841644462c7094a564d
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-01 09:26:40 +0200

    Add code for Exercise1

--- src/main/java/org/example/exercises/week1/Exercise1.java
+++ src/main/java/org/example/exercises/week1/Exercise1.java
@@ -3,6 +3,15 @@ package org.example.exercises.week1;
 import java.util.Scanner;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name");
+        Scanner obj = new Scanner(System.in); //create name object
+        String name = obj.nextLine(); //take string input
+
+        String res = name.substring(0, 1).toUpperCase() + name.substring(1);
+        System.out.println("Hej " + res + "!");
+
+    }
 
 
 }
