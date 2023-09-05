82510702468b9f45215a8bfd5ba42381937f86ad
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-05 13:18:59 +0200

    Add convertToString method

--- src/main/java/org/example/methods/Demo.java
+++ src/main/java/org/example/methods/Demo.java
@@ -10,23 +10,41 @@ public class Demo {
         int value = 5;
         printMultiplicationTable(value);
 
-        printPartOfMultiplicationTable(3,5);
+        printPartOfMultiplicationTable(3, 5);
         printWithTwoDecimals(0.12345f);
         printWithTwoDecimals(10);
 
+        int sum = add(2, 3);
+        System.out.println(sum);
+        String text = convertToString(1);
+        System.out.println(text);
     }
 
-    public static void printWithTwoDecimals(float value){
+    public static String convertToString(int number) {
+        if (number == 1)
+            return "one";
+        else if (number == 2)
+            return "two";
+        else
+            return "";
+    }
+
+    public static int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+
+    public static void printWithTwoDecimals(float value) {
         System.out.printf("%.2f\n", value);
     }
 
-    public static void printMultiplicationTable(int tableNr){
+    public static void printMultiplicationTable(int tableNr) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(tableNr * i);
         }
     }
 
-    public static void printPartOfMultiplicationTable(int tableNr, int steps){
+    public static void printPartOfMultiplicationTable(int tableNr, int steps) {
         for (int i = 1; i <= steps; i++) {
             System.out.println(tableNr * i);
         }
@@ -39,7 +57,7 @@ public class Demo {
     }
 
     public static void printMultiplicationTable3() {
-        for (int i = 1; i <= 10 ; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(3 * i);
 
         }
