2bbf65b0dde42cb05bb36a5adb7981e4343e0ca4
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-07 13:53:15 +0200

    Add test for two numbers sep with comma

--- src/main/java/org/example/tdd/Tdd.java
+++ src/main/java/org/example/tdd/Tdd.java
@@ -1,8 +1,11 @@
 package org.example.tdd;
 
+import java.util.Arrays;
+
 public class Tdd {
     public static int add(String s) {
         if (s.isEmpty()) return 0;
-        return 1;
+        var numbers = s.split(",");
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/java/org/example/tdd/TddTest.java
+++ src/test/java/org/example/tdd/TddTest.java
@@ -2,19 +2,26 @@ package org.example.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.example.tdd.Tdd.add;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TddTest {
 
     @Test
     void givenEmptyStringThenReturnZero() {
-        int answer = Tdd.add("");
+        int answer = add("");
         assertEquals(0, answer);
     }
 
     @Test
     void givenString1ThenReturn1() {
-        int answer = Tdd.add("1");
+        int answer = add("1");
         assertEquals(1, answer);
     }
+
+    @Test
+    void givenStringWithTwoNumbersThenReturnSum() {
+        assertEquals(3, add("1,2"));
+    }
+
 }
