cfd287619fc23cb9637e39a8009f3d05e3b81216
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-08 11:15:15 +0200

    Add Exercise4

---
+++ src/main/java/org/example/exercises/week2/Exercise4.java
@@ -0,0 +1,22 @@
+package org.example.exercises.week2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise4 {
+
+    public Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Locale swedishLocale = new Locale("sv","SE");
+        Locale.setDefault(swedishLocale);
+        Exercise4 exercise4 = new Exercise4();
+
+
+    }
+}
