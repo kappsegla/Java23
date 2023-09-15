83a1f3ee49c3fffb69aacafd5cd813b48c945a19
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-15 09:42:46 +0200

    Week3 Exercise3

--- src/main/java/org/example/exercises/week3/Exercise2.java
+++ src/main/java/org/example/exercises/week3/Exercise2.java
@@ -1,11 +1,14 @@
 package org.example.exercises.week3;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class  Exercise2 {
     public static void main(String[] args) {
-        String[] fruit = {"Apple", "Banana", "Orange", "Grape fruit"};
+        String[] fruit = {"Apple", "Banana", "Orange", "Grape fruit", "Banana"};
         String[] editedArr = removeElement(fruit, 1);
+        System.out.println(Arrays.toString(editedArr));
+        String[] editedArr2 = removeElement(fruit, "Banana");
         System.out.println(Arrays.toString(editedArr));
     }
 
@@ -35,4 +38,38 @@ public class  Exercise2 {
         }
         return newArray;
     }
+
+    public static String[] removeElement(String[] array, String wordToRemove){
+        int indexOfEditedArr = array.length;
+        for(String s : array){
+            if(s.equalsIgnoreCase(wordToRemove)){
+                indexOfEditedArr --;
+            }
+        }
+        String [] editedString = new String[indexOfEditedArr];
+        int newIndex = 0;
+        for(String word : array){
+            if(!word.equalsIgnoreCase(wordToRemove)){
+                editedString[newIndex] = word;
+                newIndex++;
+            }
+        }
+        return editedString;
+
+    }
+
+    
+    public static String[] removeString(String[] array, String stringToRemove) {
+        return Arrays.stream(array)
+                .filter(string -> !string.equals(stringToRemove))
+                .toArray(String[]::new);
+    }
+    public static String[] removeString(String[] array, int index) {
+        return IntStream.range(0, array.length)
+                .filter(i -> i != index)
+                .mapToObj(i -> array[i])
+                .toArray(String[]::new);
+    }
+
+
 }
