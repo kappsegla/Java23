cab02eace3afafa14277f4b8f6441d55ff80a2c8
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-15 09:07:23 +0200

    Week 3 exercise1

--- src/main/java/org/example/exercises/week3/Exercise1.java
+++ src/main/java/org/example/exercises/week3/Exercise1.java
@@ -3,6 +3,24 @@ package org.example.exercises.week3;
 public class Exercise1 {
 
     public static void main(String[] args) {
+        int[] numbers = {1, 6, 8, 10};
+        //array skapat med ett antal fasta värden
 
+        try {
+            checkEvenNumbers(numbers);
+            System.out.println("Alla tal är jämna.");
+        }
+        catch (RuntimeException e) {
+            System.out.println("Det finns udda tal i din array.");
+
+        }
     }
+    public static void checkEvenNumbers(int[] numbers) {
+        for (int num : numbers){
+            if (num % 2 !=0) {
+                throw new RuntimeException();
+            }
+        }
+    }
+
 }
