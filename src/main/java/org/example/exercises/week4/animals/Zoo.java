e86e5c11d06862580206a4b0d766a74fdb98d6b9
Martin Blomberg <martin.blomberg@outlook.com>
2023-09-22 13:23:23 +0200

    Add forEach to Zoo

--- src/main/java/org/example/exercises/week4/animals/Zoo.java
+++ src/main/java/org/example/exercises/week4/animals/Zoo.java
@@ -1,7 +1,10 @@
 package org.example.exercises.week4.animals;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.function.Consumer;
 
 public class Zoo implements Iterable<Animal> {
     private Animal[] animals = new Animal[10];
@@ -21,11 +24,26 @@ public class Zoo implements Iterable<Animal> {
 
 
     public static void main(String[] args) {
+//        List<String> strings = new ArrayList<>();
+//        strings.add("A");
+//        strings.add("B");
+//        strings.add("C");
+//        strings.forEach( (s) -> System.out.println(s));
         Zoo zoo = new Zoo();
         zoo.add(new Cat(1));
         zoo.add(new Dog(2));
         for (Animal animal : zoo) {
             System.out.println(animal);
+        }
+        zoo.forEach((animal) -> System.out.println(animal));
+        zoo.forEach(System.out::println);
+    }
+
+    @Override
+    public void forEach(Consumer<? super Animal> action) {
+        for (Animal a : animals) {
+            if (a != null)
+                action.accept(a);
         }
     }
 
