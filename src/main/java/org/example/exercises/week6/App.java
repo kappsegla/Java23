975c5ce05ee50582295e83e6530570a064dcc5d2
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-01 18:28:20 +0200

    Exercise6 code updates

--- src/main/java/org/example/exercises/week6/App.java
+++ src/main/java/org/example/exercises/week6/App.java
@@ -17,14 +17,13 @@ public class App {
                 .forEach(System.out::println);
 
         // TODO
+
 //        List all distinct project in non-ascending order.
 //        Print full name of any employee whose firstName starts with ‘A’.
 //        List of all employee who joined in year 2023 (year to be extracted from employee id i.e., 1st 4 characters)
 //        Sort employees based on firstName, for same firstName sort by salary.
 //        Print names of all employee with 3rd highest salary. (generalise it for nth highest salary).
-//        Print min salary.
 //        Print list of all employee with min salary.
-//        List of people working on more than 2 projects.
 //        Count of total laptops assigned to the employees.
 //        Count of all projects with Robert Downey Jr as PM.
 //        List of all projects with Robert Downey Jr as PM.
