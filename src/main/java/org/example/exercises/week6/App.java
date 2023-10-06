44db5552bca8c3de6161da1a1ffb5e3ad660a0f2
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-06 09:39:38 +0200

    Exercise6 question 2

--- src/main/java/org/example/exercises/week6/App.java
+++ src/main/java/org/example/exercises/week6/App.java
@@ -2,6 +2,8 @@ package org.example.exercises.week6;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Stream;
 
 public class App {
     static List<Employee> employeeList = new ArrayList<>();
@@ -11,13 +13,16 @@ public class App {
         EmployeeFactory employeeFactory = new EmployeeFactory();
         employeeList = employeeFactory.getAllEmployee();
 
-        // TODO: try implementing more queries against the List<Employee>
-        salaryGreatherThan(2000000);
-
-        //employeeList.stream().filter(x -> x.salary() > 1000000)
-        //      .forEach(x -> System.out.println(x.firstName() + " " + x.lastName() + " Har över 1000000 lön"));
+        //salaryGreatherThan(2000000);
+        //numberOfEmployees();
 
 
+
+
+    }
+
+    private static void numberOfEmployees() {
+        System.out.println("Number of employees: " + employeeList.stream().count());
     }
 
     private static void salaryGreatherThan(double minSalary) {
@@ -27,5 +32,8 @@ public class App {
                 .forEach(highSalaryEmployees::add);
         System.out.println("Employees with greater salary than " + minSalary + ":");
         highSalaryEmployees.forEach(employees -> System.out.println(employees.firstName() + " " + employees.lastName()));
+
+        //employeeList.stream().filter(x -> x.salary() > 1000000)
+        //      .forEach(x -> System.out.println(x.firstName() + " " + x.lastName() + " Har över 1000000 lön"));
     }
 }
