765a167a3336dbfd5b75d5535149b97bf3bb0c19
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-10 11:06:19 +0200

    Add FileDemo read

--- src/main/java/org/example/files/FileDemo.java
+++ src/main/java/org/example/files/FileDemo.java
@@ -1,12 +1,10 @@
 package org.example.files;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.nio.file.attribute.FileTime;
-import java.time.ZoneId;
+import java.util.stream.Stream;
 
 public class FileDemo {
     public static void main(String[] args) {
@@ -16,20 +14,21 @@ public class FileDemo {
         System.out.println(path);
         System.out.println(Files.exists(path));
         System.out.println(Files.exists(Path.of(userHome)));
-        try {
-            if (!Files.exists(path)) {
-                Files.createFile(path);
-            }
-        } catch (IOException e) {
-            System.out.println("Couldn't create file: " + e.getMessage());
-        }
-        try {
-            Files.writeString(path, "Hello there, åäö\n");
+//        try {
+//            if (!Files.exists(path)) {
+//                Files.createFile(path);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Couldn't create file: " + e.getMessage());
+//        }
+        try (Stream<String> stringStream = Files.lines(path)) {
+            //Files.writeString(path, "Hello there, åäö\n");
             //Files.writeString(path, "Hello there, åäö\n", StandardOpenOption.APPEND);
+            //String fileContent = Files.readString(path);
+            //System.out.println(fileContent);
+            stringStream.filter(s -> !s.startsWith("G")).forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Couldn't write to file: " + e.getMessage());
+            System.out.println("Couldn't access file: " + e.getMessage());
         }
-
-
     }
 }
