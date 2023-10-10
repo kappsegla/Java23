fc62e49bae790d09e001c7b19560e902a690bd65
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-10 10:23:35 +0200

    Add FileDemo save

--- src/main/java/org/example/files/FileDemo.java
+++ src/main/java/org/example/files/FileDemo.java
@@ -1,6 +1,12 @@
 package org.example.files;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileTime;
+import java.time.ZoneId;
 
 public class FileDemo {
     public static void main(String[] args) {
@@ -8,7 +14,22 @@ public class FileDemo {
         System.out.println(userHome);
         Path path = Path.of(userHome, "readme.txt");
         System.out.println(path);
-        System.out.println(java.nio.file.Files.exists(path));
+        System.out.println(Files.exists(path));
+        System.out.println(Files.exists(Path.of(userHome)));
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+        } catch (IOException e) {
+            System.out.println("Couldn't create file: " + e.getMessage());
+        }
+        try {
+            Files.writeString(path, "Hello there, åäö\n");
+            //Files.writeString(path, "Hello there, åäö\n", StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println("Couldn't write to file: " + e.getMessage());
+        }
+
 
     }
 }
