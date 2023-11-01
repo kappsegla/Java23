e35f2076b806a3974157732c517a5618a9dd0032
Martin Blomberg <martin.blomberg@outlook.com>
2023-11-01 10:52:26 +0100

    Add code for sockets

--- src/main/java/org/example/network/Client.java
+++ src/main/java/org/example/network/Client.java
@@ -1,4 +1,31 @@
 package org.example.network;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 public class Client {
+    public static void main(String[] args) {
+
+        //Ansluta sig till en server, behöver veta
+        //Serverns ip address : 127.0.0.1, 192.168.1.179, localhost
+        //Serverns port nummer 16-bit: 6000
+
+        try (Socket socket = new Socket("192.168.1.179",6000)) {
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            outputStream.writeUTF("Hello there from client!");
+            outputStream.flush();
+            ObjectInputStream inputStream =  new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+            String text = inputStream.readUTF();
+            System.out.println(text);
+
+        }catch (IOException e){
+
+        }
+
+
+    }
 }
--- src/main/java/org/example/network/Server.java
+++ src/main/java/org/example/network/Server.java
@@ -1,4 +1,37 @@
 package org.example.network;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread.ofVirtual().start(() -> handleConnection(clientSocket));
+            }
+        } catch (IOException e) {
+            System.out.println("Network error: " + e.getMessage());
+        }
+    }
+
+    private static void handleConnection(Socket clientSocket) {
+        try {
+            ObjectInputStream inputStream = new ObjectInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+
+            String text = null;
+            text = inputStream.readUTF();
+
+            System.out.println(text);
+
+            ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            outputStream.writeUTF("Hello there");
+            outputStream.flush();
+        } catch (IOException e) {
+
+        }
+    }
 }
