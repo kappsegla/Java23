7530aff26ca38d133dd0656d16b1504af0bf9a1c
Martin Blomberg <martin.blomberg@outlook.com>
2023-11-03 10:08:27 +0100

    Add decorator

--- src/main/java/org/example/pizza/decorator/ToppingDecorator.java
+++ src/main/java/org/example/pizza/decorator/ToppingDecorator.java
@@ -3,4 +3,9 @@ package org.example.pizza.decorator;
 import org.example.pizza.Pizza;
 
 public abstract class ToppingDecorator extends Pizza {
+    private final Pizza pizza;
+
+    public ToppingDecorator(Pizza pizza){
+        this.pizza  = pizza;
+    }
 }
